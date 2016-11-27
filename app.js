d0e07a4fe91c3ae05b60b872db60cebd7c61a0a5
aymenr <aymenrizwan93@gmail.com>
2016-11-27 17:19:35 +0500

    added config.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -31,6 +31,11 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+
+
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
